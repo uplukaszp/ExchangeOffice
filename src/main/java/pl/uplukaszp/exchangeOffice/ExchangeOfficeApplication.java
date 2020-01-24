2bf404290069d4f24faae17ea6c3c1ec5a571081
Łukasz <uplukaszp1@gmail.com>
2020-01-24 20:45:32 +0100

    Init commit

---
+++ src/main/java/pl/uplukaszp/exchangeOffice/ExchangeOfficeApplication.java
@@ -0,0 +1,13 @@
+package pl.uplukaszp.exchangeOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/uplukaszp/exchangeOffice/ExchangeOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uplukaszp.exchangeOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
